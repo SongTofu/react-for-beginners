e00f41dbc88794f6c6b2cbd85d0cbbfb8e98e0c6
Yejin Song <yejsong@c5r2s7.42seoul.kr>
2022-07-23 17:49:24 +0900

    Ch.5 finish

---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+import styled from "./Button.module.css";
+
+function Button({ text }) {
+  return <button className={styled.btn}>{text}</button>;
+}
+export default Button;
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired
+};
